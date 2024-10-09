5977920b24b76b431add5399c78c620f98decb29
Amanda Rodrigues <139911046+amanjonson22@users.noreply.github.com>
2024-10-09 16:41:25 -0300

    Update UsuarioController.swift

--- TesteArquitetura/TesteArquitetura/Controller/UsuarioController.swift
+++ TesteArquitetura/TesteArquitetura/Controller/UsuarioController.swift
@@ -11,6 +11,6 @@ class UsuarioController: ObservableObject {
     @Published var usuario: Usuario?
 
     func fetchUser() {
-        usuario = Usuario(nome: "Taskid", desc: "App para incentivar crianças a realizar tarefas domésticas")
+        usuario = Usuario(nome: "Duolingo", desc: "App para aprender novos idiomas")
     }
 }
